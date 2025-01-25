7be5601aab12af67829b48d56f384c546c69cba4
Hariharan Manochrasa <harishmanochrasa@gmail.com>
2025-01-25 19:21:35 +0530

    button event listener

--- country-code-selector.js
+++ country-code-selector.js
@@ -9,3 +9,24 @@ const input = document.querySelector("#number-intp");
   },
     loadUtils: () => import("https://cdn.jsdelivr.net/npm/intl-tel-input@25.2.1/build/js/utils.js"),
   });
+
+  var button = document.querySelector("#wa-form-sbmt"); 
+  var form = document.querySelector("#wf-form-whatsapp-general"); 
+  
+  button.addEventListener("click", function (event) {
+    event.preventDefault(); 
+    
+    reset(); 
+
+    if (!input.value.trim()) {
+      alert("Kindly enter your phone number");
+    } else if (iti.isValidNumber()) {
+      console.log("Phone number is valid");
+      form.submit();
+    } else {
+      const errorCode = iti.getValidationError();
+      const msg = errorMap[errorCode] || "Invalid number";
+      console.log(msg);
+      alert("Kindly enter a valid phone number");
+    }
+  });
